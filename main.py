51a6dd8ee1bb518512d98837bf6ffe6fd103f93e
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2024-11-18 19:35:31 +0300

    тест push

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ def print_hi(name):
     # 043388b1d6e303185f974f28dfcfb5796117ad63
 
 
+def coord(x, y):
+    return x * y
+
+
 def calc(a, b):
     return a + b
 
 
 print(calc(10, 15))
-
+print(coord(4, 5))
 if __name__ == '__main__':
     print_hi('PyCharm')
